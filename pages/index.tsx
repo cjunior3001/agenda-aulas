2d9bdd7b22dea181f4e0b75663f466b08d70021b
cjunior3001 <jrpractice@hotmail.com>
2021-08-30 21:51:10 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+const IndexPage: NextPage = () => {
+  return(
+    <div>
+      <div className="py-20">
+        <h1 className="text-5x1 text-center text-accent-1">
+          Next + tailwind
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default IndexPage;
